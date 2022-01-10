6162bdba1e06169b2e167f3c22029e618dcabc3f
IceRaptor <gkitts@gmail.com>
2022-01-10 14:57:38 -0500

    - Hide turrets until the body fo the dropship is completely visible

--- MonsterMashup/MonsterMashup/Patch/AbstractActorPatch.cs
+++ MonsterMashup/MonsterMashup/Patch/AbstractActorPatch.cs
@@ -3,6 +3,8 @@ using Harmony;
 using IRBTModUtils;
 using IRBTModUtils.Extension;
 using MonsterMashup.UI;
+using System;
+using UnityEngine;
 
 namespace MonsterMashup.Patch
 {
@@ -60,7 +62,82 @@ namespace MonsterMashup.Patch
     [HarmonyPatch(typeof(AbstractActor), "OnPlayerVisibilityChanged")]
     static class AbstractActor_OnPlayerVisibilityChanged
     {
-        static bool Prefix(AbstractActor __instance)
+        static void Postfix(AbstractActor __instance, VisibilityLevel newLevel)
+        {
+            Mod.Log.Info?.Write($"AA:OnPlayerVisibilityChanged:POST INVOKED for: {__instance.DistinctId()}");
+
+            if (newLevel == VisibilityLevel.LOSFull && Mod.Config.DeveloperOptions.EnableFootprintVis)
+            {
+                if (ModState.FootprintVisuals.TryGetValue(__instance.DistinctId(), out FootprintVisualization footprintVis))
+                {
+                    Mod.Log.Info?.Write($"Showing footprint visualization for actor: {__instance.DistinctId()}");
+                    footprintVis.Show();
+                }
+            }
+        }
+    }
+
+
+    [HarmonyPatch(typeof(AbstractActor), "CanDetectPositionNonCached")]
+    static class AbstractActor_CanDetectPositionNonCached
+    {
+        //CanDetectPositionNonCached(Vector3 worldPos, AbstractActor target)
+        static bool Prefix(AbstractActor __instance, Vector3 worldPos, AbstractActor target, ref bool __result)
+        {
+            Mod.Log.Info?.Write($"AA:CanDetectPositionNonCached INVOKED for: {__instance.DistinctId()}");
+
+            if (__instance == null || target == null) return true;
+            Mod.Log.Debug?.Write($"AA:CanDetectPositionNonCached from {target.DistinctId()} to {target.DistinctId()}");
+
+            string parentUID = target.StatCollection.GetValue<string>(ModStats.Linked_Parent_Actor_UID);
+            if (!string.IsNullOrEmpty(parentUID))
+            {
+                Mod.Log.Debug?.Write($"Target: {target.DistinctId()} has linked parent: {parentUID}");
+                AbstractActor parent = SharedState.Combat.FindActorByGUID(parentUID);
+                if (parent != null && parent.GameRep != null && !parent.GameRep.VisibleToPlayer)
+                {
+                    Mod.Log.Debug?.Write($"  --parent is not visible, skipping.");
+                    __result = false;
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+    }
+
+    [HarmonyPatch(typeof(AbstractActor), "CanSeeTargetAtPositionNonCached")]
+    static class AbstractActor_CanSeeTargetAtPositionNonCached
+    {
+        //CanSeeTargetAtPositionNonCached(Vector3 worldPos, Quaternion rotation, AbstractActor target)
+        static bool Prefix(AbstractActor __instance, Vector3 worldPos, Quaternion rotation, AbstractActor target, ref bool __result)
+        {
+            Mod.Log.Info?.Write($"AA:CanSeeTargetAtPositionNonCached INVOKED for: {__instance.DistinctId()}");
+
+            if (__instance == null || target == null) return true;
+            Mod.Log.Debug?.Write($"AA:CanSeeTargetAtPositionNonCached from {target.DistinctId()} to {target.DistinctId()}");
+
+            string parentUID = target.StatCollection.GetValue<string>(ModStats.Linked_Parent_Actor_UID);
+            if (!string.IsNullOrEmpty(parentUID))
+            {
+                Mod.Log.Debug?.Write($"Target: {target.DistinctId()} has linked parent: {parentUID}");
+                AbstractActor parent = SharedState.Combat.FindActorByGUID(parentUID);
+                if (parent != null && parent.GameRep != null && !parent.GameRep.VisibleToPlayer)
+                {
+                    Mod.Log.Debug?.Write($"  --parent is not visible, skipping.");
+                    __result = false;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+    /*
+     *        static bool Prefix(AbstractActor __instance)
         {
             Mod.Log.Info?.Write($"AA:OnPlayerVisibilityChanged:PRE INVOKED for: {__instance.DistinctId()}");
             if (__instance != null)
@@ -82,19 +159,35 @@ namespace MonsterMashup.Patch
 
             return true;
         }
+     */
 
-        static void Postfix(AbstractActor __instance, VisibilityLevel newLevel)
+    [HarmonyPatch(typeof(LineOfSight), "GetVisibilityToTargetWithPositionsAndRotations")]
+    [HarmonyPatch(new Type[] { typeof(AbstractActor), typeof(Vector3), typeof(ICombatant), typeof(Vector3), typeof(Quaternion) })]
+    public static class LineOfSight_GetVisibilityToTargetWithPositionsAndRotations
+    {
+        public static bool Prefix(LineOfSight __instance, ref VisibilityLevel __result,
+            AbstractActor source, Vector3 sourcePosition, ICombatant target, Vector3 targetPosition, Quaternion targetRotation)
         {
-            Mod.Log.Info?.Write($"AA:OnPlayerVisibilityChanged:POST INVOKED for: {__instance.DistinctId()}");
 
-            if (newLevel == VisibilityLevel.LOSFull && Mod.Config.DeveloperOptions.EnableFootprintVis)
+
+            if (__instance == null || target == null) return true;
+            Mod.Log.Debug?.Write($"AA:CanSeeTargetAtPositionNonCached from {target.DistinctId()} to {target.DistinctId()}");
+
+            string parentUID = target.StatCollection.GetValue<string>(ModStats.Linked_Parent_Actor_UID);
+            if (!string.IsNullOrEmpty(parentUID))
             {
-                if (ModState.FootprintVisuals.TryGetValue(__instance.DistinctId(), out FootprintVisualization footprintVis))
+                Mod.Log.Debug?.Write($"Target: {target.DistinctId()} has linked parent: {parentUID}");
+                AbstractActor parent = SharedState.Combat.FindActorByGUID(parentUID);
+                if (parent != null && parent.GameRep != null && !parent.GameRep.VisibleToPlayer)
                 {
-                    Mod.Log.Info?.Write($"Showing footprint visualization for actor: {__instance.DistinctId()}");
-                    footprintVis.Show();
+                    Mod.Log.Debug?.Write($"  --parent is not visible, skipping.");
+                    __result = VisibilityLevel.None;
+                    return false;
                 }
             }
+
+            return true;
         }
     }
+
 }

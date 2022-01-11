79c682eb4bb9899428f38b6d5d78c8979b6adff1
IceRaptor <gkitts@gmail.com>
2022-01-11 12:14:33 -0500

    no message

---
+++ MonsterMashup/MonsterMashup/Patch/PathingUtilPatch.cs
@@ -0,0 +1,30 @@
+﻿using BattleTech;
+using Harmony;
+using IRBTModUtils;
+using IRBTModUtils.Extension;
+using MonsterMashup.UI;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MonsterMashup.Patch
+{
+    [HarmonyPatch(typeof(PathingUtil), "DoesMovementLineCollide")]
+    [HarmonyAfter("io.mission.customunits")]
+    static class PathingUtil_DoesMovementLineCollide
+    {
+        // bool DoesMovementLineCollide(AbstractActor thisActor, List<AbstractActor> actors, Vector3 start, Vector3 end, out AbstractActor collision, float meleeRangeMultiplier)
+        static void Postfix(ref bool __result, AbstractActor thisActor, List<AbstractActor> actors, Vector3 start, Vector3 end)
+        {
+
+            Mod.Log.Info?.Write($" -- SourceActor: {thisActor.DistinctId()} has radius: {thisActor.Radius} with doesCollide: {__result}");
+            foreach (AbstractActor targetActor in actors)
+            {
+                Mod.Log.Info?.Write($" ---- TargetActor: {targetActor.DistinctId()} has radius: {targetActor.Radius}");
+            }
+            
+
+        }
+    }
+
+}

2fe9d96a583f61fedb92a3e850a6d961745503e0
IceRaptor <gkitts@gmail.com>
2024-03-15 11:31:08 -0400

    - Rename LinkedTurretComponent to LinkedActorComponent

---
+++ MonsterMashup/MonsterMashup/Component/Components.cs
@@ -0,0 +1,35 @@
+﻿using CustomComponents;
+using System;
+using System.Collections.Generic;
+
+namespace MonsterMashup.Component
+{
+    [CustomComponent("MM_LinkedActor")]
+    public class LinkedActorComponent : SimpleCustomComponent
+    {
+        public string CUVehicleDefId = "";
+        public string PilotDefId = "";
+
+        public string AttachPoint = "";
+
+        public bool LinkInitiative = true;
+    }
+
+    [CustomComponent("MM_WeaponController")]
+    public class WeaponControllerComponent: SimpleCustomChassis
+    {
+#pragma warning disable CS0649
+        public AttachMapping[] AttachMappings;
+    }
+
+    public class AttachMapping
+    {
+        public string Transform = "";
+#pragma warning disable CS0649
+        public int[] HardpointID;
+        public bool AlignRepresentation = true;
+        public int RestrictedFiringArc = 0;
+    }
+
+
+}

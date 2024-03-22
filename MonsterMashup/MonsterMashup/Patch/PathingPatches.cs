317d2e84f68ebb8596409254ad99f2e02e0d90c7
IceRaptor <gkitts@gmail.com>
2024-03-21 22:48:27 -0400

    - Allow friendly units to move through multi-hex units

--- MonsterMashup/MonsterMashup/Patch/PathingPatches.cs
+++ MonsterMashup/MonsterMashup/Patch/PathingPatches.cs
@@ -64,12 +64,30 @@ namespace MonsterMashup.Patch
                 if (actor.Radius > 12f)
                 {
 
-                    // List<Vector3> GetGridPointsAroundPointWithinRadius(Vector3 cartesianPoint, int radius)
+                    Vector3 centerPoint = SharedState.Combat.HexGrid.GetClosestPointOnGrid(actor.CurrentIntendedPosition);
+                    Vector3 posHexGridPoint = SharedState.Combat.HexGrid.GetClosestPointOnGrid(pos);
+                    if (centerPoint == posHexGridPoint)
+                    {
+                        Mod.Log.Debug?.Write($" -- hexPoint: {centerPoint} conflicts with pos: {pos}, marking actor: {actor.DistinctId()} as colliding.");
+                        occupyingActor = actor;
+                        __result = true;
+                        return;
+                    }
+
+                    // Check if friendly
+                    bool isFriendly = SharedState.Combat.HostilityMatrix.IsFriendly(unit.team, actor.team);
+                    if (isFriendly)
+                    {
+                        Mod.Log.Debug?.Write($" -- unit: {unit.DistinctId()} is friendly to actor: {actor.DistinctId()}, marking as no-colliding.");
+                        __result = false;
+                        return;
+                    }
+
+                    // Neutral or Hostil, check all hexes in our radius
                     int radius = (int)Mathf.Ceil(actor.Radius);
                     List<Vector3> hexGridPoints = SharedState.Combat.HexGrid.GetGridPointsWithinCartesianDistance(actor.CurrentIntendedPosition, radius);
-                    Mod.Log.Trace?.Write($" -- actor: {actor.DistinctId()} occupies: {hexGridPoints.Count} hex points from radius: {radius}");
-
-                    Vector2 axialCoords = SharedState.Combat.HexGrid.CartesianToHexAxial(pos);
+                    Mod.Log.Debug?.Write($" -- actor: {actor.DistinctId()} occupies: {hexGridPoints.Count} hex points from radius: {radius}");
+                    Vector3 axialCoords = SharedState.Combat.HexGrid.CartesianToHexAxial(pos);
                     Vector2 vector = SharedState.Combat.HexGrid.HexAxialRound(axialCoords);
 
                     foreach (Vector3 hexGridPoint in hexGridPoints)

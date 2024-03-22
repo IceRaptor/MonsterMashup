5bc1578373c5393f416e366fb3a7cf56516876d8
IceRaptor <gkitts@gmail.com>
2024-03-21 22:05:06 -0400

    - Prevent linkedActors from being see if the player has no visibility to the parent

--- MonsterMashup/MonsterMashup/Patch/LineOfSightPatch.cs
+++ MonsterMashup/MonsterMashup/Patch/LineOfSightPatch.cs
@@ -9,8 +9,10 @@ namespace MonsterMashup.Patch
     [HarmonyPatch(new Type[] { typeof(AbstractActor), typeof(Vector3), typeof(ICombatant), typeof(Vector3), typeof(Quaternion) })]
     static class LineOfSight_GetVisibilityToTargetWithPositionsAndRotations
     {
+        // check to see if our parent's visibility is none. If so, hide our own visiblity.
         static void Prefix(ref bool __runOriginal, LineOfSight __instance, ref VisibilityLevel __result, ICombatant target)
         {
+            Mod.Log.Info?.Write($"GVTTWPAR Target: {target.DistinctId()}");
             if (!__runOriginal) return;
 
             if (__instance == null || target == null) return;
@@ -18,16 +20,50 @@ namespace MonsterMashup.Patch
             string parentUID = target.StatCollection.GetValue<string>(ModStats.Linked_Parent_Actor_UID);
             if (!string.IsNullOrEmpty(parentUID))
             {
-                Mod.Log.Trace?.Write($"Target: {target.DistinctId()} has linked parent: {parentUID}");
+                Mod.Log.Info?.Write($"Target: {target.DistinctId()} has linked parent: {parentUID}");
                 AbstractActor parent = SharedState.Combat.FindActorByGUID(parentUID);
-                if (parent != null && parent.GameRep != null && !parent.GameRep.VisibleToPlayer)
+                if (parent != null && parent.GameRep != null)
                 {
-                    Mod.Log.Trace?.Write($"  --parent is not visible, skipping.");
-                    __result = VisibilityLevel.None;
-                    __runOriginal = false;
+                    VisibilityLevel parentVisibility = SharedState.Combat.LocalPlayerTeam.VisibilityToTarget(parent);
+                    if (!parent.GameRep.VisibleToPlayer || parentVisibility < VisibilityLevel.LOSFull)
+                    {
+                        Mod.Log.Info?.Write($"  --parent is not visible, skipping.");
+                        __result = VisibilityLevel.None;
+                        __runOriginal = false;
+                        return;
+                    }
                 }
             }
 
+        }
+
+        // check to see if our parent's visibility is greater than ours. If so, up our visibility to the same level as the parent
+        static void Postfix(LineOfSight __instance, ref VisibilityLevel __result, ICombatant target)
+        {
+            Mod.Log.Info?.Write($"GVTTWPAR Target: {target.DistinctId()}");
+
+            if (__instance == null || target == null) return;
+
+            string parentUID = target.StatCollection.GetValue<string>(ModStats.Linked_Parent_Actor_UID);
+            if (!string.IsNullOrEmpty(parentUID))
+            {
+                Mod.Log.Info?.Write($"Target: {target.DistinctId()} has linked parent: {parentUID}");
+                AbstractActor parent = SharedState.Combat.FindActorByGUID(parentUID);
+                if (parent != null && parent.GameRep != null)
+                {
+                    VisibilityLevel parentVisibility = SharedState.Combat.LocalPlayerTeam.VisibilityToTarget(parent);
+                    if (parentVisibility == VisibilityLevel.LOSFull)
+                    {
+                        __result = VisibilityLevel.LOSFull;
+                        Mod.Log.Info?.Write($"  -- upping visibility to parent's level of: {parentVisibility}");
+                    }
+                    else
+                    {
+                        __result = VisibilityLevel.None;
+                        Mod.Log.Info?.Write($"  -- Hiding sensor blip");
+                    }
+                }
+            }
         }
     }
 
